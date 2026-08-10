09d93dc33fdf7952a3dfc41e84993cbad122e9cc
Gowtham <jayavarapugowtham@gmail.com>
2026-08-10 22:09:15 +0530

    added regression part into my code

--- KNN/knn_classification_and_regression.py
+++ KNN/knn_classification_and_regression.py
@@ -5,10 +5,9 @@ from collections import Counter
 
 
 class KNN:
-    def __init__(self,K=3,task="classification",weighted = False):
+    def __init__(self,K=3,task="classification"):
         self.K = K
         self.task = task
-        self.weighted = weighted
 
         self.X_train = None
         self.y_train = None
@@ -16,7 +15,7 @@ class KNN:
         self.X_train =np.array(x) 
         self.y_train = np.array(y)
     def calculate_distance(self,new_set):
-        return np.sqrt(np.sum((np.array(new_set)-np.array(self.X_train))**2),axis=1)
+        return np.sqrt(np.sum((np.array(new_set)-np.array(self.X_train))**2,axis=1),)
     def calculate_neighbors(self,locs):
         neighbors = []
         for i in locs:
@@ -28,8 +27,12 @@ class KNN:
         distances = self.calculate_distance(X)
         k_indices = np.argsort(distances)[:self.K]
         neighbors = self.calculate_neighbors(k_indices)
-        predicted_class = Counter(neighbors).most_common(1)[0][0]
-        return predicted_class
+        
+        if self.task=="classification":
+            predicted_class = Counter(neighbors).most_common(1)[0][0]
+            return predicted_class
+        else:
+            return np.mean(neighbors)
     def predict_multiple(self,X_test):
         predictions = []
         for i in X_test:
